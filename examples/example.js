76da45cebbd4f83c59e74164aaed672a34ff451d
Luc Vauvillier <luc.vauvillier@gmail.com>
2017-10-19 18:02:17 +0200

    New parsing loop with jpeg images extraction

--- examples/example.js
+++ examples/example.js
@@ -2,9 +2,14 @@ var DJIParser = require("../index");
 var fs = require("fs");
 
 var parser = new DJIParser();
+var imageIndex = 1;
 
 parser.on("OSD", function(obj) {
   console.log(obj.getHeight());
+});
+
+parser.on("IMAGE", function(obj) {
+  fs.writeFileSync("image"+(imageIndex++)+".jpg", obj.buffer, "binary");
 });
 
 parser.parse(fs.readFileSync("DJIFlightRecord_2015-12-29_[19-05-48].txt"));
